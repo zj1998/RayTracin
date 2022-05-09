a781fa2cc861107c8ada094753a58eedbcf3d094
gates James <1046303600@qq.com>
2022-05-09 16:52:47 +0800

    final

--- Project5/vec3.h
+++ Project5/vec3.h
@@ -1,7 +1,7 @@
 #pragma once
 #include<cmath>
 #include<iostream>
-
+#include"rtweekend.h"
 
 
 class vec3
@@ -14,6 +14,18 @@ public:
 	double y() const{ return e[1]; }
 	double z() const{ return e[2]; }
 
+    inline static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+    
+    bool near_zero()const {
+        const auto s = 1e-8;
+        return(fabs(e[1]) < s) && (fabs(e[2]) < s) && (fabs(e[3])< s);
+    }
 	vec3 operator-() const{ return { -e[0],-e[1] ,-e[2] }; }
 	double operator[](int i) const {
 		return e[i];
@@ -68,7 +80,13 @@ inline vec3 operator+(vec3 &v1,vec3 &v2) {
     return sum;
 }
 // vec3 Utility Functions
-
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
 inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
     return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
@@ -111,4 +129,11 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector( vec3 v) {
     return v / v.length();
+}
+vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+vec3 reflect(const vec3 &v,const vec3 &n) {
+    return v - 2 * dot(v, n) * n;
 }
